03a2bd7b02e229fe27b4d56a851b4b30680d2e98
iProgYou <zack.musicteacher@gmail.com>
2021-03-01 21:42:24 -0800

    Challenge completed

--- mileage/auto_seeker.rb
+++ mileage/auto_seeker.rb
@@ -19,7 +19,22 @@ class AutoSeeker
   end
 
   def self.median_mileage autos
-    prices = autos.collect(&:mileage).sort
+    # there is a nil value in the dataset that is causing .sort to fail
+    # I am just rejecting any nil values I find and then sorting the remaining elements
+    prices = autos.collect(&:mileage).reject(&:nil?).sort
     (prices[(prices.length - 1) / 2].to_f + prices[prices.length / 2].to_f) / 2.0
   end
+
+  def self.price_range_filter(autos,low,high)
+    raise "Invalid Range" if low > high
+    autos.select { |auto| auto.price >= low && auto.price <= high }
+
+  end
+
+  def self.fuel_type_filter(autos, fuel_type)
+    # The README states that I should write out this method, but this logic can be handled
+    #   by the filter method by using .filter(:fuel, "gas") on an AutoSeeker instance
+    return autos.select { |auto| auto.fuel == fuel_type }
+  end
+
 end
